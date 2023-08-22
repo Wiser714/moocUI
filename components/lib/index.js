0ce77d1d8b4e8fbde053c90bf02904162a578b28
Wiser714 <wiser714962290@163.com>
2023-08-22 23:00:03 +0800

    111

--- components/lib/index.js
+++ components/lib/index.js
@@ -1,30 +1,5 @@
-// import Demo from "./demo";
-// import Card from "./card";
-
-// const components = {
-//   Demo,
-//   Card,
-// };
-
-// const install = function (Vue) {
-//   if (install.installed) return;
-//   console.log('components -->', components)
-//   Object.keys(components).forEach((key) => {
-//     Vue.component(components[key].name, components[key]);
-//   });
-// };
-
-// const API = {
-//   install,
-// };
-
-// export default API;
-
-/// ----
-import Demo from './demo';
-import Card from './card';
-
-// import { version } from '../../package.json';
+import Demo from "./demo";
+import Card from "./card";
 
 const components = {
   Demo,
@@ -33,15 +8,13 @@ const components = {
 
 const install = function (Vue) {
   if (install.installed) return;
-  Object.keys(components).forEach(key => {
+  Object.keys(components).forEach((key) => {
     Vue.component(components[key].name, components[key]);
-  })
+  });
 };
 
-
-
 const API = {
-  install
+  install,
 };
 
 export default API;
